bbd6ed9a0dfa9db058996c5eda3a1ea86219f6a2
Chitlam-Glaz <on9on9juju@gmail.com>
2025-10-03 18:33:33 +0800

    new_page

--- code.js
+++ code.js
@@ -7,7 +7,29 @@ var button = document.querySelector("#cv");
 //attach and event when the user clicks it
 button.addEventListener("click", myfunction);
 
-//create 
 function myfunction(){
     alert("Let me tell you more about myself!")
+}
+
+var button = document.querySelector("#welcome");
+
+button.addEventListener("click", welcomefunction);
+
+function welcomefunction(){
+    alert("Welcome to my Personal Webpage!")
+}
+
+var mynode = document.createElement("div");
+
+//change basic attributes
+mynode.id = "my_works";
+mynode.innerHTML = "The work is exhibitions";
+mynode.style.color = "blue";
+
+//add event listener
+mynode.addEventListener("click", welcomeToWork1);
+document.querySelector("#my_works").appendChild(mynode);
+
+function welcomeToWork1(){
+    mynode.innerHTML = "Thank you for your interest in my work1!"
 }
